5bb142dcf1d6602f1ddeb4d6fccd698b7eb0115c
Daniel Molina <danielmolina19.dm@gmail.com>
2021-08-13 20:33:29 -0500

    modificando archivos

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('movies.json')
+const router = jsonServer.router('data.json')
 const middlewares = jsonServer.defaults()
 
 
